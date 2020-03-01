e202d45e4149130536430644d5e5dbb511466bc1
Vincent Nys <vincentnys@gmail.com>
2020-03-01 13:59:45 +0100

    Oplossingske

---
+++ python-var.py
@@ -0,0 +1,8 @@
+seconden_per_minuut = 60
+minuten_per_uur = 60
+uren_per_dag = 24
+dagen_per_week = 7
+boodschap = "Er zijn zo veel seconden in een week:"
+berekening = seconden_per_minuut * minuten_per_uur * uren_per_dag * dagen_per_week
+print(boodschap)
+print(berekening)
