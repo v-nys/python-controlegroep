a8ce90da0bcda01202f45e0fbcc01a1dec47fd11
Vincent Nys <vincentnys@gmail.com>
2020-03-27 11:02:29 +0100

    Strip van de regel.

--- python_spam_file.py
+++ python_spam_file.py
@@ -2,11 +2,10 @@ import python_spam_regel_functie
 
 def file_is_spam():
     answer = input("Welke file moet gecontroleerd worden?\n")
-    path = answer.split(" ")
     with open(answer) as fh:
         is_spam = False
         for line in fh.readlines():
-            is_spam = is_spam or python_spam_regel_functie.check_spam_line(line)
+            is_spam = is_spam or python_spam_regel_functie.check_spam_line(line.strip())
     if is_spam:
         print("Deze file bevat spam!")
     else:
