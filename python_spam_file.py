13027fd76c6954180721ac987969a3e85ce42c50
Vincent Nys <vincentnys@gmail.com>
2020-03-27 10:10:06 +0100

    Modeloplossing nieuwe formulering.

--- python_spam_file.py
+++ python_spam_file.py
@@ -1,12 +1,13 @@
-answer = input("Welke file moet gecontroleerd worden?\n")
-path = answer.split(" ")
-with open(answer) as fh:
-    is_spam = False
-    for line in fh.readlines():
-        for word in line.split(" "):
-            if word == "lottery" or word == "inheritance" or word == "viagra":
-                is_spam = True
-if is_spam:
-    print("Dit is spam.")
-else:
-    print("Dit is geen spam.")
+import python_spam_regel_functie
+
+def file_is_spam:
+    answer = input("Welke file moet gecontroleerd worden?\n")
+    path = answer.split(" ")
+    with open(answer) as fh:
+        is_spam = False
+        for line in fh.readlines():
+            is spam = is_spam or python_spam_regel_functie.check_spam_line(line)
+    if is_spam:
+        print("Dit is spam.")
+    else:
+        print("Dit is geen spam.")
