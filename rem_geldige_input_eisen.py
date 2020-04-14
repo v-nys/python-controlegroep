fdf7288faea6bd1ab5b11f63b6310d8472a7aac3
Vincent Nys <vincentnys@gmail.com>
2020-04-14 12:00:07 +0200

    Opzettelijk fout.

--- rem_geldige_input_eisen.py
+++ rem_geldige_input_eisen.py
@@ -2,4 +2,4 @@ def vraag_ja_of_neen():
     answer = None
     while not (answer == "ja" or answer == "neen"):
         answer = input("Gelieve ja of neen te antwoorden.\n")
-    print("Bedankt voor je input!")
+    print("Bedankt voor je inpu")
