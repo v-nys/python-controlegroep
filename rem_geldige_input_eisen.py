ed87b21b59b5e98ae669bda6b00332e3588eaa25
Vincent Nys <vincentnys@gmail.com>
2020-04-14 11:57:22 +0200

    Om remote test te doen.

---
+++ rem_geldige_input_eisen.py
@@ -0,0 +1,5 @@
+def vraag_ja_of_neen():
+    answer = None
+    while not (answer == "ja" or answer == "neen"):
+        answer = input("Gelieve ja of neen te antwoorden.\n")
+    print("Bedankt voor je input!")
