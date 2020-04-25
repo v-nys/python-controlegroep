6ba16b559ed1cde936eb224108a84ec11e33fb13
Vincent Nys <vincentnys@gmail.com>
2020-04-25 19:24:34 +0200

    Update voor overeenstemming.

--- python_spam_file_functie.py
+++ python_spam_file_functie.py
@@ -4,5 +4,5 @@ def file_is_spam(answer):
     with open(answer) as fh:
         is_spam = False
         for line in fh.readlines():
-            is_spam = is_spam or python_spam_regel_functie.check_spam_line(line.strip())
+            is_spam = is_spam or python_spam_regel_functie.line_is_spam(line.strip())
     return is_spam
