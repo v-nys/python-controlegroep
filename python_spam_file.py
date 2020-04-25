c84cf90b11b06c33809c0f19d8d8022dfd21c849
Vincent Nys <vincentnys@gmail.com>
2020-04-25 19:22:40 +0200

    In overeenstemming met instructies.

--- python_spam_file.py
+++ python_spam_file.py
@@ -5,7 +5,7 @@ def file_is_spam():
     with open(answer) as fh:
         is_spam = False
         for line in fh.readlines():
-            is_spam = is_spam or python_spam_regel_functie.check_spam_line(line.strip())
+            is_spam = is_spam or python_spam_regel_functie.line_is_spam(line.strip())
     if is_spam:
         print("Deze file bevat spam!")
     else:
